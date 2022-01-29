f595dd2e0f7e091fa2fe8280ba8356d9ab884038
kenzo7664 <olaotanemmanuel1@gmail.com>
2022-01-29 21:52:47 +0100

    pr conflict changes

--- src/components/app/finance/Collections/CollectionList.tsx
+++ src/components/app/finance/Collections/CollectionList.tsx
@@ -1,66 +1,51 @@
-import React from 'react';
-import DataTable from 'react-data-table-component';
-import { DebounceInput } from 'react-debounce-input';
-import { ToastContainer } from 'react-toastify';
-import { TableMenu } from '../../../../styles/global';
-import Button from '../../../buttons/Button';
-import Input from '../../../inputs/basic/Input';
-import { CollectionSchema } from '../../ModelSchema';
-import { PageWrapper } from '../../styles'
+import React from "react";
+import DataTable from "react-data-table-component";
+
+import { TableMenu } from "../../../../styles/global";
+import Input from "../../../inputs/basic/Input";
+import { PageWrapper } from "../../styles";
+import { columnHead, rowData } from "./data";
 
 interface Props {
   handleCreate?: () => void;
-  handleSearch: (_event) => void;
   onRowClicked?: (
-    _row: { id: any; description: string; client: string; amount: number; mode: string },
-    _event: any
+    row: {
+      id: any;
+      name: string;
+      client: string;
+      amount: string;
+      mode: string;
+    },
+    event: any
   ) => void;
-  items: any[];
 }
 
-const Collections: React.FC<Props> = ({
-  handleSearch,
-  onRowClicked,
-  items,
-}) => {
+const Collections: React.FC<Props> = ({ onRowClicked }) => {
   return (
     <PageWrapper>
       <h2>Collections</h2>
 
       <TableMenu>
-      <div className="inner-table">
-          <Input
-            placeholder="Search here"
-            label="Search here"
-            onChange={handleSearch}
-          />
-          <DebounceInput
-            className="input is-small "
-            type="text"
-            placeholder="Search Collections"
-            minLength={1}
-            debounceTimeout={400}
-            onChange={(e) => handleSearch(e.target.value)}
-          />
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <span>Filter by</span>
-            <i className="bi bi-chevron-down" />
+        <div className='inner-table'>
+          <Input placeholder='Search here' label='Search here' />
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <span>Filer by</span>
+            <i className='bi bi-chevron-down'></i>
           </div>
         </div>
-
       </TableMenu>
 
-      <div style={{ width: '100%', height: '600px', overflow: 'auto' }}>
+      <div style={{ width: "100%", height: "600px", overflow: "auto" }}>
         <DataTable
-          title="Collections"
-          columns={CollectionSchema}
-          data={items}
+          title='Collections'
+          columns={columnHead}
+          data={rowData}
           selectableRows
           pointerOnHover
           highlightOnHover
           striped
           onRowClicked={onRowClicked}
-          style={{ overflow: 'hidden' }}
+          style={{ overflow: "hidden" }}
         />
       </div>
     </PageWrapper>
