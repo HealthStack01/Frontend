7037149dabbbf8582968ef99580b10ef40215c0c
John Alade <john.alade@ng.airtel.com>
2022-02-12 15:49:13 +0100

    refactorigns

--- src/pages/auth/Signup.tsx
+++ src/pages/auth/Signup.tsx
@@ -6,7 +6,6 @@ import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 import {
-  EmployeeSchema,
   getResolver,
   OnboardingEmployeeSchema,
   OrganisationSchema,
