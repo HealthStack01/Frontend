26773c7be37b0956399f1e2df7573f884ce63547
JohnTheBeloved <topzy20@gmail.com>
2022-03-12 22:20:28 +0100

    Added new files for Attend Refactor

---
+++ src/components/app/clients/Appointments/components/InfoBox.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import { FlexBox } from '../../../../../styles/global';
+import RoundButton from '../../../../buttons/Button/RoundButton';
+
+const InfoBox = () => {
+  return (
+    <>
+      <FlexBox className="left">
+        <FlexBox className="align-left">
+          <FlexBox className="row">
+            <h6>Specific Information</h6>
+            <RoundButton />
+          </FlexBox>
+
+          <small>null</small>
+        </FlexBox>
+        <FlexBox className="align-left">
+          <FlexBox className="row">
+            <h6>Allergies</h6>
+            <RoundButton />
+          </FlexBox>
+
+          <small>null</small>
+        </FlexBox>
+        <FlexBox className="align-left">
+          <FlexBox className="row">
+            <h6>Mobidities</h6>
+            <RoundButton />
+          </FlexBox>
+
+          <small>null</small>
+        </FlexBox>
+        <FlexBox className="align-left">
+          <FlexBox className="row">
+            <h6>Disabilities</h6>
+            <RoundButton />
+          </FlexBox>
+
+          <small>null</small>
+        </FlexBox>
+      </FlexBox>
+    </>
+  );
+};
+
+export default InfoBox;
---
+++ src/components/app/clients/Appointments/components/TabOverview.tsx
@@ -0,0 +1,48 @@
+import { ButtonGroup, TableMenu } from '../../../../../styles/global';
+import Button from '../../../../buttons/Button';
+import Input from '../../../../inputs/basic/Input';
+import { toDurationString, toShortDate } from '../../../DateUtils';
+import { DetailsWrapper } from '../../../styles';
+import DocumentViewer from './DocumentViewer';
+
+const TabOverview = ({ documentations, onNewPrescription, onNewLabOrder }) => {
+  return (
+    <>
+      <TableMenu>
+        <div
+          className="inner-table"
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            height: '40px',
+          }}
+        >
+          <Input placeholder="Search here" label="Search here" size="small" />
+        </div>
+        <ButtonGroup>
+          <Button background="#Fafafa" color="#222" showicon={true} onClick={onNewPrescription}>
+            <i className="bi bi-plus-circle"></i> New Prescription
+          </Button>
+          <Button background="#Fafafa" color="#222" showicon={true} onClick={onNewLabOrder}>
+            <i className="bi bi-plus-circle"></i> New Lab Order
+          </Button>
+        </ButtonGroup>
+      </TableMenu>
+      <div>
+        {documentations.map((documentation, index) => {
+          const { createdAt, documentname, createdByname, location, facilityname, status } = documentation;
+          const time = toDurationString(createdAt);
+          const day = toShortDate(createdAt);
+          const description = `${time} on ${day}: ${documentname} by ${createdByname}  ${location}, ${facilityname}  ${status}`;
+          return (
+            <DetailsWrapper title={description} key={index}>
+              <DocumentViewer document={documentation} />
+            </DetailsWrapper>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default TabOverview;
