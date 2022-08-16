5342e92f2f914f2572fe6e436a5c242d52d5649a
inuoluwadunsimi <danielolaoladeinde@gmail.com>
2022-08-16 01:04:42 +0100

    implement calendar feature

---
+++ src/components/utilities/CalendarIndex.ts
@@ -0,0 +1,19 @@
+import {
+  Appointments,
+  Scheduler,
+  WeekView,
+} from '@devexpress/dx-react-scheduler-material-ui';
+import Paper from '@mui/material/Paper';
+import * as React from 'react';
+
+import CurrentAppointment from './CurrentAppointmrnt';
+
+// export default ()  => (
+//   <Paper>
+//     <Scheduler data={CurrentAppointment} height={660}>
+//       <WeekView startDayHour={9} endDayHour={19} />
+//       <Appointments />
+//     </Scheduler>
+//   </Paper>
+// );
+  
