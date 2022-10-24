da0dca1d4c8783924005c9a323defaf440f41c3c
Idowu Motunrayo <motungeorge6@gmail.com>
2022-10-24 09:37:46 +0100

    Motun made a commit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
-import React from 'react';
-<<<<<<< HEAD
-// import { createRoot } from 'react-dom/client';
-=======
-/* import { createRoot } from 'react-dom/client'; */
->>>>>>> 56038078bf20f61ffc1bc93419c1cb7ef21b7a79
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-/* import reportWebVitals from './reportWebVitals'; */
+import React from "react";
 
+/* import { createRoot } from 'react-dom/client'; */
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+/* import reportWebVitals from './reportWebVitals'; */
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
