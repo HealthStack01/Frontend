b1ac95bf5692075e1594b52a23b36b5f11020c47
Idowu Motunrayo <motungeorge6@gmail.com>
2022-11-11 21:10:31 +0100

    routes,invoice and proposal

--- src/hsmodules/CRM/proposal.js
+++ src/hsmodules/CRM/proposal.js
@@ -834,34 +834,6 @@ export function ProposalList({ showModal, setShowModal }) {
                   <h2 style={{ margin: "0 10px", fontSize: "0.95rem" }}>
                     Lead
                   </h2>
-                  <DatePicker
-                    selected={startDate}
-                    onChange={(date) => handleDate(date)}
-                    dateFormat="dd/MM/yyyy"
-                    placeholderText="Filter By Date"
-                    isClearable
-                  />
-                  {/* <SwitchButton /> */}
-                  <Switch>
-                    <button
-                      value={value}
-                      onClick={() => {
-                        setValue("list");
-                      }}
-                      style={value === "list" ? activeStyle : {}}
-                    >
-                      <BsList style={{ fontSize: "1rem" }} />
-                    </button>
-                    <button
-                      value={value}
-                      onClick={() => {
-                        setValue("grid");
-                      }}
-                      style={value === "grid" ? activeStyle : {}}
-                    >
-                      <BsFillGridFill style={{ fontSize: "1rem" }} />
-                    </button>
-                  </Switch>
                 </div>
 
                 {handleCreateNew && (
