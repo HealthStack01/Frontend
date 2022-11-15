5664453334e3546b53a55f230df622d43df7fa02
Sulaimon Olaniran <oladipupo.olaniran47@gmail.com>
2022-11-15 23:22:25 +0100

    update healthplan

--- src/hsmodules/ManagedCare/HealthPlan.js
+++ src/hsmodules/ManagedCare/HealthPlan.js
@@ -1,37 +1,37 @@
 /* eslint-disable */
-import React, { useState, useContext, useEffect, useRef } from "react";
-import { Route, useNavigate, Link, NavLink } from "react-router-dom";
+import React, {useState, useContext, useEffect, useRef} from "react";
+import {Route, useNavigate, Link, NavLink} from "react-router-dom";
 import client from "../../feathers";
-import { DebounceInput } from "react-debounce-input";
-import { useForm } from "react-hook-form";
+import {DebounceInput} from "react-debounce-input";
+import {useForm} from "react-hook-form";
 //import {useNavigate} from 'react-router-dom'
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
-import { UserContext, ObjectContext } from "../../context";
-import { toast } from "bulma-toast";
-import { formatDistanceToNowStrict, format, subDays, addDays } from "date-fns";
+import {UserContext, ObjectContext} from "../../context";
+import {toast} from "bulma-toast";
+import {formatDistanceToNowStrict, format, subDays, addDays} from "date-fns";
 import DatePicker from "react-datepicker";
 import LocationSearch from "../helpers/LocationSearch";
 import EmployeeSearch from "../helpers/EmployeeSearch";
 import BillServiceCreate from "../Finance/BillServiceCreate";
 import "react-datepicker/dist/react-datepicker.css";
 
-import { PageWrapper } from "../../ui/styled/styles";
-import { TableMenu } from "../../ui/styled/global";
+import {PageWrapper} from "../../ui/styled/styles";
+import {TableMenu} from "../../ui/styled/global";
 import FilterMenu from "../../components/utilities/FilterMenu";
 import Button from "../../components/buttons/Button";
 import CustomTable from "../../components/customtable";
 import Switch from "../../components/switch";
-import { BsFillGridFill, BsList } from "react-icons/bs";
+import {BsFillGridFill, BsList} from "react-icons/bs";
 import CalendarGrid from "../../components/calender";
 import ModalBox from "../../components/modal";
-import { Box, Grid, Button as MuiButton } from "@mui/material";
+import {Box, Grid, Button as MuiButton} from "@mui/material";
 import DebouncedInput from "../Appointment/ui-components/inputs/DebouncedInput";
-import { MdCancel } from "react-icons/md";
+import {MdCancel} from "react-icons/md";
 // eslint-disable-next-line
 const searchfacility = {};
 
 export default function HealthPlan() {
-  const { state } = useContext(ObjectContext); //,setState
+  const {state} = useContext(ObjectContext); //,setState
   // eslint-disable-next-line
   const [selectedClient, setSelectedClient] = useState();
   const [selectedAppointment, setSelectedAppointment] = useState();
@@ -45,9 +45,9 @@ export default function HealthPlan() {
   );
 }
 
-export function AppointmentCreate({ showModal, setShowModal }) {
-  const { state, setState } = useContext(ObjectContext);
-  const { register, handleSubmit, setValue } = useForm(); //, watch, errors, reset
+export function AppointmentCreate({showModal, setShowModal}) {
+  const {state, setState} = useContext(ObjectContext);
+  const {register, handleSubmit, setValue} = useForm(); //, watch, errors, reset
   const [error, setError] = useState(false);
   const [success, setSuccess] = useState(false);
   const [success1, setSuccess1] = useState(false);
@@ -61,7 +61,7 @@ export function AppointmentCreate({ showModal, setShowModal }) {
   const [facility, setFacility] = useState();
   const ClientServ = client.service("appointments");
   //const navigate=useNavigate()
-  const { user } = useContext(UserContext); //,setUser
+  const {user} = useContext(UserContext); //,setUser
   // eslint-disable-next-line
   const [currentUser, setCurrentUser] = useState();
   const [selectedClient, setSelectedClient] = useState();
@@ -83,15 +83,15 @@ export function AppointmentCreate({ showModal, setShowModal }) {
             shouldDirty: true
         })
     } */
-  const handleChangeType = async (e) => {
+  const handleChangeType = async e => {
     await setAppointment_type(e.target.value);
   };
 
-  const handleChangeStatus = async (e) => {
+  const handleChangeStatus = async e => {
     await setAppointment_status(e.target.value);
   };
 
-  const getSearchfacility = (obj) => {
+  const getSearchfacility = obj => {
     setClientId(obj._id);
     setChosen(obj);
     //handleRow(obj)
@@ -106,7 +106,7 @@ export function AppointmentCreate({ showModal, setShowModal }) {
             shouldDirty: true
         }) */
   };
-  const getSearchfacility1 = (obj) => {
+  const getSearchfacility1 = obj => {
     setLocationId(obj._id);
     setChosen1(obj);
 
@@ -116,7 +116,7 @@ export function AppointmentCreate({ showModal, setShowModal }) {
       setChosen1();
     }
   };
-  const getSearchfacility2 = (obj) => {
+  const getSearchfacility2 = obj => {
     setPractionerId(obj._id);
     setChosen2(obj);
 
@@ -151,7 +151,7 @@ export function AppointmentCreate({ showModal, setShowModal }) {
     setError(false);
     setSuccess(false);
     setShowModal(false),
-      setState((prevstate) => ({
+      setState(prevstate => ({
         ...prevstate,
         AppointmentModule: {
           selectedAppointment: {},
@@ -191,7 +191,7 @@ export function AppointmentCreate({ showModal, setShowModal }) {
     console.log(data);
 
     ClientServ.create(data)
-      .then((res) => {
+      .then(res => {
         //console.log(JSON.stringify(res))
         e.target.reset();
         setAppointment_type("");
@@ -214,7 +214,7 @@ export function AppointmentCreate({ showModal, setShowModal }) {
         setSuccess2(false);
         // showBilling()
       })
-      .catch((err) => {
+      .catch(err => {
         toast({
           message: "Error creating Appointment " + err,
           type: "is-danger",
@@ -260,7 +260,7 @@ export function AppointmentCreate({ showModal, setShowModal }) {
               <MdCancel
                 onClick={() => {
                   setShowModal(false),
-                    setState((prevstate) => ({
+                    setState(prevstate => ({
                       ...prevstate,
                       AppointmentModule: {
                         selectedAppointment: {},
@@ -308,13 +308,13 @@ export function AppointmentCreate({ showModal, setShowModal }) {
                   <label
                     className=" is-small"
                     key={c}
-                    style={{ fontSize: "16px", fontWeight: "bold" }}
+                    style={{fontSize: "16px", fontWeight: "bold"}}
                   >
                     <input
                       type="radio"
                       value={c}
                       name="appointmentClass"
-                      {...register("appointmentClass", { required: true })}
+                      {...register("appointmentClass", {required: true})}
                       style={{
                         border: "1px solid #0364FF",
                         transform: "scale(1.5)",
@@ -333,7 +333,7 @@ export function AppointmentCreate({ showModal, setShowModal }) {
               <div className="field">
                 <input
                   name="start_time"
-                  {...register("start_time", { required: true })}
+                  {...register("start_time", {required: true})}
                   type="datetime-local"
                   style={{
                     border: "1px solid #0364FF",
@@ -393,7 +393,7 @@ export function AppointmentCreate({ showModal, setShowModal }) {
               <textarea
                 className="input is-small"
                 name="appointment_reason"
-                {...register("appointment_reason", { required: true })}
+                {...register("appointment_reason", {required: true})}
                 type="text"
                 placeholder="Appointment Reason"
                 rows="10"
@@ -425,7 +425,7 @@ export function AppointmentCreate({ showModal, setShowModal }) {
             <Grid item xs={12} sm={12} md={4} lg={3}>
               <Button
                 type="button"
-                onClick={(e) => e.target.reset()}
+                onClick={e => e.target.reset()}
                 style={{
                   backgroundColor: "#ffffff",
                   width: "100%",
@@ -444,7 +444,7 @@ export function AppointmentCreate({ showModal, setShowModal }) {
   );
 }
 
-export function HealthPlanList({ showModal, setShowModal }) {
+export function HealthPlanList({showModal, setShowModal}) {
   // const { register, handleSubmit, watch, errors } = useForm();
   // eslint-disable-next-line
   const [error, setError] = useState(false);
@@ -459,9 +459,9 @@ export function HealthPlanList({ showModal, setShowModal }) {
   // eslint-disable-next-line
   const [selectedClient, setSelectedClient] = useState(); //
   // eslint-disable-next-line
-  const { state, setState } = useContext(ObjectContext);
+  const {state, setState} = useContext(ObjectContext);
   // eslint-disable-next-line
-  const { user, setUser } = useContext(UserContext);
+  const {user, setUser} = useContext(UserContext);
   const [startDate, setStartDate] = useState(new Date());
   const [selectedAppointment, setSelectedAppointment] = useState();
   const [loading, setLoading] = useState(false);
@@ -472,7 +472,7 @@ export function HealthPlanList({ showModal, setShowModal }) {
       selectedAppointment: {},
       show: "create",
     };
-    await setState((prevstate) => ({
+    await setState(prevstate => ({
       ...prevstate,
       AppointmentModule: newClientModule,
     }));
@@ -481,25 +481,25 @@ export function HealthPlanList({ showModal, setShowModal }) {
       selectedClient: {},
       show: "create",
     };
-    await setState((prevstate) => ({ ...prevstate, ClientModule: newClient }));
+    await setState(prevstate => ({...prevstate, ClientModule: newClient}));
     setShowModal(true);
   };
 
-  const handleRow = async (Client) => {
+  const handleRow = async Client => {
     setShowModal(true);
     await setSelectedAppointment(Client);
     const newClientModule = {
       selectedAppointment: Client,
       show: "detail",
     };
-    await setState((prevstate) => ({
+    await setState(prevstate => ({
       ...prevstate,
       AppointmentModule: newClientModule,
     }));
   };
   //console.log(state.employeeLocation)
 
-  const handleSearch = (val) => {
+  const handleSearch = val => {
     const field = "firstname";
     //  console.log(val)
 
@@ -588,14 +588,14 @@ export function HealthPlanList({ showModal, setShowModal }) {
       query.locationId = state.employeeLocation.locationId;
     }
 
-    ClientServ.find({ query: query })
-      .then((res) => {
+    ClientServ.find({query: query})
+      .then(res => {
         console.log(res);
         setFacilities(res.data);
         setMessage(" Client  fetched successfully");
         setSuccess(true);
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
         setMessage("Error fetching Client, probable network issues " + err);
         setError(true);
@@ -617,7 +617,7 @@ export function HealthPlanList({ showModal, setShowModal }) {
       //   stuff.locationId = state.employeeLocation.locationId;
       // }
 
-      const findClient = await ClientServ.find({ query: stuff });
+      const findClient = await ClientServ.find({query: stuff});
 
       await setFacilities(findClient.data);
       console.log(findClient.data);
@@ -649,15 +649,15 @@ export function HealthPlanList({ showModal, setShowModal }) {
                     console.log(user)
                     getFacilities(user) */
     }
-    ClientServ.on("created", (obj) => handleCalendarClose());
-    ClientServ.on("updated", (obj) => handleCalendarClose());
-    ClientServ.on("patched", (obj) => handleCalendarClose());
-    ClientServ.on("removed", (obj) => handleCalendarClose());
+    ClientServ.on("created", obj => handleCalendarClose());
+    ClientServ.on("updated", obj => handleCalendarClose());
+    ClientServ.on("patched", obj => handleCalendarClose());
+    ClientServ.on("removed", obj => handleCalendarClose());
     const newClient = {
       selectedClient: {},
       show: "create",
     };
-    setState((prevstate) => ({ ...prevstate, ClientModule: newClient }));
+    setState(prevstate => ({...prevstate, ClientModule: newClient}));
     return () => {};
   }, []);
   const handleCalendarClose = async () => {
@@ -677,12 +677,12 @@ export function HealthPlanList({ showModal, setShowModal }) {
     //   query.locationId = state.employeeLocation.locationId;
     // }
 
-    const findClient = await ClientServ.find({ query: query });
+    const findClient = await ClientServ.find({query: query});
 
     await setFacilities(findClient.data);
   };
 
-  const handleDate = async (date) => {
+  const handleDate = async date => {
     setStartDate(date);
   };
 
@@ -792,23 +792,23 @@ export function HealthPlanList({ showModal, setShowModal }) {
     },
   ];
 
-  const returnCell = (status) => {
+  const returnCell = status => {
     // if (status === "approved") {
     //   return <span style={{color: "green"}}>{status}</span>;
     // }
     // else if
     switch (status.toLowerCase()) {
       case "active":
-        return <span style={{ color: "#17935C" }}>{status}</span>;
+        return <span style={{color: "#17935C"}}>{status}</span>;
 
       case "ongoing":
-        return <span style={{ color: "#0364FF" }}>{status}</span>;
+        return <span style={{color: "#0364FF"}}>{status}</span>;
 
       case "declined":
-        return <span style={{ color: "#ED0423" }}>{status}</span>;
+        return <span style={{color: "#ED0423"}}>{status}</span>;
 
       case "pending":
-        return <span style={{ color: "#EF9645" }}>{status}</span>;
+        return <span style={{color: "#EF9645"}}>{status}</span>;
 
       default:
         break;
@@ -829,7 +829,7 @@ export function HealthPlanList({ showModal, setShowModal }) {
       name: "Name of Plan",
       key: "name_of_plan",
       description: "Name of Plan",
-      selector: (row) => row.name_of_plan,
+      selector: row => row.name_of_plan,
       sortable: true,
       required: true,
       inputType: "TEXT",
@@ -838,7 +838,7 @@ export function HealthPlanList({ showModal, setShowModal }) {
       name: "Category",
       key: "category",
       description: "Category",
-      selector: (row) => row.category,
+      selector: row => row.category,
       sortable: true,
       required: true,
       inputType: "TEXT",
@@ -866,7 +866,7 @@ export function HealthPlanList({ showModal, setShowModal }) {
 
   const conditionalRowStyles = [
     {
-      when: (row) => row.status === "approved",
+      when: row => row.status === "approved",
       style: {
         color: "red",
         "&:hover": {
@@ -875,7 +875,7 @@ export function HealthPlanList({ showModal, setShowModal }) {
       },
     },
     {
-      when: (row) => row.status === "ongoing",
+      when: row => row.status === "ongoing",
       style: {
         color: "rgba(0,0,0,.54)",
         "&:hover": {
@@ -884,7 +884,7 @@ export function HealthPlanList({ showModal, setShowModal }) {
       },
     },
     {
-      when: (row) => row.status === "pending",
+      when: row => row.status === "pending",
       style: {
         color: "pink",
         "&:hover": {
@@ -893,7 +893,7 @@ export function HealthPlanList({ showModal, setShowModal }) {
       },
     },
     {
-      when: (row) => row.status === "declined",
+      when: row => row.status === "declined",
       style: {
         color: "purple",
         backgroundColor: "green",
@@ -910,10 +910,10 @@ export function HealthPlanList({ showModal, setShowModal }) {
         <>
           <div className="level">
             <PageWrapper
-              style={{ flexDirection: "column", padding: "0.6rem 1rem" }}
+              style={{flexDirection: "column", padding: "0.6rem 1rem"}}
             >
               <TableMenu>
-                <div style={{ display: "flex", alignItems: "center" }}>
+                <div style={{display: "flex", alignItems: "center"}}>
                   {handleSearch && (
                     <div className="inner-table">
                       <FilterMenu onSearch={handleSearch} />
@@ -938,7 +938,7 @@ export function HealthPlanList({ showModal, setShowModal }) {
                       }}
                       style={value === "list" ? activeStyle : {}}
                     >
-                      <BsList style={{ fontSize: "1rem" }} />
+                      <BsList style={{fontSize: "1rem"}} />
                     </button>
                     <button
                       value={value}
@@ -947,7 +947,7 @@ export function HealthPlanList({ showModal, setShowModal }) {
                       }}
                       style={value === "grid" ? activeStyle : {}}
                     >
-                      <BsFillGridFill style={{ fontSize: "1rem" }} />
+                      <BsFillGridFill style={{fontSize: "1rem"}} />
                     </button>
                   </Switch>
                 </div>
@@ -964,14 +964,14 @@ export function HealthPlanList({ showModal, setShowModal }) {
                     onClick={handleCreateNew}
                   >
                     <AddCircleOutlineIcon
-                      sx={{ marginRight: "5px" }}
+                      sx={{marginRight: "5px"}}
                       fontSize="small"
                     />
                     Add Health plan
                   </MuiButton>
                 )}
               </TableMenu>
-              <div style={{ width: "100%", height: "700px", overflow: "auto" }}>
+              <div style={{width: "100%", height: "700px", overflow: "auto"}}>
                 {value === "list" ? (
                   <CustomTable
                     title={""}
