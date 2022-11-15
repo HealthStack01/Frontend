27d7ce57e03d40921962b22bed431d49814b1f7b
daregoodness1997 <daregoodness@gmail.com>
2022-11-15 14:31:54 +0100

    dashboard

--- src/components/menuitem/index.tsx
+++ src/components/menuitem/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 
 import { ListItem, Lists, MenuList } from './style';
@@ -16,43 +17,52 @@ const MenuItem: React.FC<MenuItemProps> = ({
   subMenus,
   onClick,
   iconClassName,
+  to,
 }) => {
   const [expand, setExpand] = useState<boolean>(false);
 
   return (
-    <ListItem onClick={onClick}>
-      <MenuList onClick={() => setExpand(!expand)} className="menu-item">
-        <div className="menu-label">
-          <div className="menu-label-right">
-            <div className="menu-icon">
-              <i className={iconClassName} />
+    <NavLink
+      to={to}
+      style={{
+        color: 'white',
+        textDecoration: 'none',
+      }}
+    >
+      <ListItem onClick={onClick}>
+        <MenuList onClick={() => setExpand(!expand)} className='menu-item'>
+          <div className='menu-label'>
+            <div className='menu-label-right'>
+              <div className='menu-icon'>
+                <i className={iconClassName} />
+              </div>
+              <span style={{ fontSize: '0.75rem' }}>{name}</span>
             </div>
-            <span style={{ fontSize: '0.75rem' }}>{name}</span>
+
+            {subMenus.length ? <i className='bi bi-chevron-down' /> : ''}
           </div>
 
-          {subMenus.length ? <i className="bi bi-chevron-down" /> : ''}
-        </div>
-
-        {subMenus && subMenus.length ? (
-          <Lists className={`sub-menu ${expand ? 'active' : ''}`}>
-            {subMenus.map((menu, index) => (
-              <ListItem key={index}>
-                <NavLink
-                  to={menu.to}
-                  style={{
-                    color: 'white',
-                    textDecoration: 'none',
-                    fontSize: '0.9rem',
-                  }}
-                >
-                  {menu.name}
-                </NavLink>
-              </ListItem>
-            ))}
-          </Lists>
-        ) : null}
-      </MenuList>
-    </ListItem>
+          {subMenus && subMenus.length ? (
+            <Lists className={`sub-menu ${expand ? 'active' : ''}`}>
+              {subMenus.map((menu, index) => (
+                <ListItem key={index}>
+                  <NavLink
+                    to={menu.to}
+                    style={{
+                      color: 'white',
+                      textDecoration: 'none',
+                      fontSize: '0.9rem',
+                    }}
+                  >
+                    {menu.name}
+                  </NavLink>
+                </ListItem>
+              ))}
+            </Lists>
+          ) : null}
+        </MenuList>
+      </ListItem>
+    </NavLink>
   );
 };
 
