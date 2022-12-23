69702c163d08cd4905bb0eaac056d565c63ee1dc
Amodu Kehinde <amodukehindee@gmail.com>
2022-12-23 04:54:14 +0100

    fix client tasks

--- src/hsmodules/Patientportal/components/search/SearchItems.js
+++ src/hsmodules/Patientportal/components/search/SearchItems.js
@@ -1,30 +1,33 @@
 import {Stack,Box,Typography,Card,CardMedia,CardContent
     ,Rating,CardActions,Drawer,Paper,CardHeader,Avatar,IconButton} from '@mui/material'
     import StarIcon from '@mui/icons-material/Star';
+    import {doctorData} from "./data"
 
 export default function SearchList(){
     return(
         <Box width="30%">
-        <Card>
-            <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        action={
-        
-        <Typography variant="p" fontSize="16px" fontWeight="normal" color="text.secondary">
-          4.5 
-          <Box component="span" pt="0.5rem">
-          <StarIcon sx={{color:"gold"}} />
-          </Box>
-       </Typography>
-        }
-        title="Chorizo Paella"
-        subheader="Doctor"
-      />
-            </Card>
+          {doctorData.map((data) => (
+              <Card>
+              <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
+              {data.img}
+            </Avatar>
+          }
+          action={
+          
+          <Typography variant="p" fontSize="16px" fontWeight="normal" color="text.secondary">
+            {data.rating}
+            <Box component="span" pt="0.5rem">
+            <StarIcon sx={{color:"gold"}} />
+            </Box>
+         </Typography>
+          }
+          title={data.name}
+          subheader={data.title}
+        />
+              </Card>
+          ))}
         </Box>
     )
 }
