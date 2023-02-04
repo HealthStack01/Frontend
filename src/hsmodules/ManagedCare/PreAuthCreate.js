40fbbd65afb400ec6ff35dfbc6d021c98eb930cc
Dare Goodness <daregoodness@gmail.com>
2023-02-04 21:31:53 +0100

    preauthcreate

--- src/hsmodules/ManagedCare/PreAuthCreate.js
+++ src/hsmodules/ManagedCare/PreAuthCreate.js
@@ -7,20 +7,46 @@ import GlobalCustomButton from '../../components/buttons/CustomButton';
 import Textarea from '../../components/inputs/basic/Textarea';
 import CustomTable from '../../components/customtable';
 import { diagnosisSchema, serviceSchema } from './schema';
+import ModalBox from '../../components/modal';
 
 const PreAuthCreate = () => {
 	const { register, handleSubmit, watch, errors } = useForm();
 	const [open, setOpen] = useState(false);
+	const [type, setType] = useState();
 	const [diagnosis, setDiagnosis] = useState({});
-	const [povdiagnosis, setProvDiagnosis] = useState([]);
+	const [provdiagnosis, setProvDiagnosis] = useState([]);
 	const [service, setService] = useState({});
 
 	const diagnosisData = [];
-	const prevDiagnosisData = [];
+	const provDiagnosisData = [];
+	const serviceData = [];
+	const handleAddDiagnosis = () => {
+		provDiagnosisData.push(provdiagnosis);
+	};
+	const handleAddService = () => {
+		serviceData.push(service);
+	};
 
 	const submit = async data => {};
 	return (
 		<Box>
+			<ModalBox
+				open={open}
+				onClose={() => setOpen(false)}>
+				{type === 'service' ? (
+					<AddService
+						service={service}
+						setService={setService}
+						handleAddService={handleAddService}
+					/>
+				) : (
+					<AddDiagnosis
+						handleAddDiagnosis={handleAddDiagnosis}
+						provdiagnosis={provdiagnosis}
+						setProvDiagnosis={setProvDiagnosis}
+					/>
+				)}
+			</ModalBox>
 			<h2>Create PreAuthorization</h2>
 			<form onSubmit={handleSubmit(submit)}>
 				<Box
@@ -133,7 +159,10 @@ const PreAuthCreate = () => {
 					<Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
 						<h3>Provisional Diagnosis</h3>
 						<GlobalCustomButton
-							onClick={() => setOpen(true)}
+							onClick={() => {
+								setOpen(true);
+								setType('diagnosis');
+							}}
 							loading={false}>
 							Add Provisional Diagnosis
 						</GlobalCustomButton>
@@ -155,12 +184,15 @@ const PreAuthCreate = () => {
 					/>
 				</Box>
 
-				{/* Provisional Diagnosis Findings */}
+				{/* Services*/}
 				<Box sx={{ my: '2rem' }}>
 					<Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
 						<h3>Services</h3>
 						<GlobalCustomButton
-							onClick={() => setOpen(true)}
+							onClick={() => {
+								setOpen(true);
+								setType('services');
+							}}
 							loading={false}>
 							Add Services
 						</GlobalCustomButton>
@@ -194,7 +226,7 @@ const PreAuthCreate = () => {
 
 export default PreAuthCreate;
 
-const AddService = ({ service, setService }) => {
+const AddService = ({ service, setService, handleAddService }) => {
 	return (
 		<Box>
 			<h2>Service</h2>
@@ -227,49 +259,52 @@ const AddService = ({ service, setService }) => {
 				/>
 			</Box>
 			<Textarea
-				label='Treatment Plan'
+				label='Comment'
 				onChange={e => setService({ ...service, comment: e.target.value })}
 			/>
+			<GlobalCustomButton
+				onClick={handleAddService}
+				loading={false}>
+				Submit
+			</GlobalCustomButton>
 		</Box>
 	);
 };
 
-const AddDiagnosis = ({ service, setService }) => {
+const AddDiagnosis = ({
+	provdiagnosis,
+	setProvDiagnosis,
+	handleAddDiagnosis,
+}) => {
 	return (
 		<Box>
 			<h2>Diagnosis</h2>
-			<Box
-				sx={{
-					display: 'grid',
-					gridTemplateColumns: { xs: '1fr', lg: 'repeat(2,1fr)' },
-					gap: '1rem',
-					my: '1rem',
-				}}>
-				<Input
-					label='Service'
-					placeholder='Enter a Service'
-					onChange={e => setService({ ...service, item: e.target.value })}
-				/>
-				<Input
-					label='Unit Price'
-					placeholder='Enter a Unit Price'
-					onChange={e => setService({ ...service, unitPrice: e.target.value })}
-				/>
-				<Input
-					label='Quantity'
-					placeholder='Enter a quantity'
-					onChange={e => setService({ ...service, quantity: e.target.value })}
-				/>
-				<Input
-					label='Total Amount'
-					placeholder='Enter a total amount'
-					onChange={e => setService({ ...service, total: e.target.value })}
-				/>
-			</Box>
-			<Textarea
-				label='Treatment Plan'
-				onChange={e => setService({ ...service, comment: e.target.value })}
+			<Input
+				label='Service'
+				placeholder='Enter a Service'
+				onChange={e =>
+					setProvDiagnosis({ ...provdiagnosis, item: e.target.value })
+				}
 			/>
+			<Input
+				label='Unit Price'
+				placeholder='Enter a Unit Price'
+				onChange={e =>
+					setProvDiagnosis({ ...provdiagnosis, unitPrice: e.target.value })
+				}
+			/>
+
+			<Textarea
+				label='Comment'
+				onChange={e =>
+					setProvDiagnosis({ ...provdiagnosis, comment: e.target.value })
+				}
+			/>
+			<GlobalCustomButton
+				onClick={handleAddDiagnosis}
+				loading={false}>
+				Submit
+			</GlobalCustomButton>
 		</Box>
 	);
 };
