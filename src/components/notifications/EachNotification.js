6d070b7f26579a98719c483094c73d19e7f4f8a0
Sulaimon Olaniran <oladipupo.olaniran47@gmail.com>
2023-02-12 18:05:47 +0100

    update organization page to accomodate corporate organizations

--- src/components/notifications/EachNotification.js
+++ src/components/notifications/EachNotification.js
@@ -1,176 +1,186 @@
-import {useContext, useState} from 'react';
-import {IconButton, Menu, MenuItem, Typography} from '@mui/material';
-import {Box} from '@mui/system';
-import MessageIcon from '@mui/icons-material/Message';
-import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
-import dayjs from 'dayjs';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import Badge from '@mui/material/Badge';
-import client from '../../feathers';
-import {ObjectContext, UserContext} from '../../context';
-import {toast} from 'react-toastify';
-import {useNavigate, useLocation} from 'react-router-dom';
+import {useContext, useState} from "react";
+import {IconButton, Menu, MenuItem, Typography} from "@mui/material";
+import {Box} from "@mui/system";
+import MessageIcon from "@mui/icons-material/Message";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
+import dayjs from "dayjs";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import Badge from "@mui/material/Badge";
+import client from "../../feathers";
+import {ObjectContext, UserContext} from "../../context";
+import {toast} from "react-toastify";
+import {useNavigate, useLocation} from "react-router-dom";
 
-var relativeTime = require('dayjs/plugin/relativeTime');
+var relativeTime = require("dayjs/plugin/relativeTime");
 dayjs.extend(relativeTime);
 
 const EachNotification = ({notification, closeDrawer}) => {
-	const notificationsServer = client.service('notification');
-	const {user} = useContext(UserContext);
-	const {showActionLoader, hideActionLoader} = useContext(ObjectContext);
-	const [anchorEl, setAnchorEl] = useState(null);
+  const notificationsServer = client.service("notification");
+  const {user} = useContext(UserContext);
+  const {showActionLoader, hideActionLoader} = useContext(ObjectContext);
+  const [anchorEl, setAnchorEl] = useState(null);
 
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const handleCloseOptions = () => {
-		setAnchorEl(null);
-	};
+  const handleCloseOptions = () => {
+    setAnchorEl(null);
+  };
 
-	const handleOpenOptions = event => {
-		setAnchorEl(event.currentTarget);
-	};
+  const handleOpenOptions = event => {
+    setAnchorEl(event.currentTarget);
+  };
 
-	const markAsSeen = async () => {
-		const prevReads = notification.isRead;
-		const documentId = notification._id;
-		const userId = user.currentEmployee._id;
-		showActionLoader();
+  const markAsSeen = async () => {
+    const prevReads = notification.isRead;
+    const documentId = notification._id;
+    const userId = user.currentEmployee._id;
+    showActionLoader();
 
-		const newReads = [userId, ...prevReads];
+    const newReads = [userId, ...prevReads];
 
-		await notificationsServer
-			.patch(documentId, {isRead: newReads})
-			.then(res => {
-				hideActionLoader();
-				handleCloseOptions();
-				toast.success('Notification successfully marked as read');
-			})
-			.catch(err => {
-				console.log(err);
-				hideActionLoader();
-				toast.error(`Failed to mark notification as read ${err}`);
-			});
-	};
+    await notificationsServer
+      .patch(documentId, {isRead: newReads})
+      .then(res => {
+        hideActionLoader();
+        handleCloseOptions();
+        toast.success("Notification successfully marked as read");
+      })
+      .catch(err => {
+        console.log(err);
+        hideActionLoader();
+        toast.error(`Failed to mark notification as read ${err}`);
+      });
+  };
 
-	const viewNotification = async () => {
-		const notificationPath = notification.pageUrl || '/app';
+  const viewNotification = async () => {
+    const notificationPath = notification.pageUrl || "/app";
 
-		const prevReads = notification.isRead;
-		const documentId = notification._id;
-		const userId = user.currentEmployee._id;
-		showActionLoader();
+    const prevReads = notification.isRead;
+    const documentId = notification._id;
+    const userId = user.currentEmployee._id;
+    showActionLoader();
 
-		const newReads = [userId, ...prevReads];
+    const newReads = [userId, ...prevReads];
 
-		await notificationsServer
-			.patch(documentId, {isRead: newReads})
-			.then(res => {
-				hideActionLoader();
-				handleCloseOptions();
-				navigate(notificationPath);
-				closeDrawer();
-				//toast.success('Notification successfully marked as read');
-			});
+    await notificationsServer
+      .patch(documentId, {isRead: newReads})
+      .then(res => {
+        hideActionLoader();
+        handleCloseOptions();
+        navigate(notificationPath);
+        closeDrawer();
+        //toast.success('Notification successfully marked as read');
+      });
 
-		// console.log(location.pathname);
-	};
+    // console.log(location.pathname);
+  };
 
-	return (
-		<Box
-			sx={{
-				position: 'relative',
-			}}>
-			<Box>
-				<IconButton
-					onClick={handleOpenOptions}
-					sx={{
-						position: 'absolute',
-						right: '5px',
-						top: '0px',
-					}}>
-					<MoreHorizIcon fontSize='small' />
-				</IconButton>
-				<Menu
-					anchorEl={anchorEl}
-					id='account-menu'
-					open={Boolean(anchorEl)}
-					onClose={handleCloseOptions}
-					anchorOrigin={{horizontal: 'right', vertical: 'center'}}>
-					<MenuItem
-						sx={{fontSize: '0.8rem'}}
-						onClick={markAsSeen}>
-						Mark as Read
-					</MenuItem>
+  return (
+    <Box
+      sx={{
+        position: "relative",
+      }}
+    >
+      <Box>
+        <IconButton
+          onClick={handleOpenOptions}
+          sx={{
+            position: "absolute",
+            right: "5px",
+            top: "0px",
+          }}
+        >
+          <MoreHorizIcon fontSize="small" />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          id="account-menu"
+          open={Boolean(anchorEl)}
+          onClose={handleCloseOptions}
+          anchorOrigin={{horizontal: "right", vertical: "center"}}
+        >
+          <MenuItem sx={{fontSize: "0.8rem"}} onClick={markAsSeen}>
+            Mark as Read
+          </MenuItem>
 
-					<MenuItem
-						sx={{fontSize: '0.8rem'}}
-						onClick={viewNotification}>
-						View Notification
-					</MenuItem>
-				</Menu>
-			</Box>
+          <MenuItem sx={{fontSize: "0.8rem"}} onClick={viewNotification}>
+            View Notification
+          </MenuItem>
+        </Menu>
+      </Box>
 
-			<Box
-				sx={{
-					width: '100%',
-					padding: '10px 10px',
-					borderBottom: '1px solid #f0f0f0',
-					cursor: 'pointer',
-					':hover': {
-						backgroundColor: '#f0f0f0',
-					},
-				}}
-				onClick={viewNotification}>
-				<Box
-					sx={{
-						display: 'flex',
-						alignItems: 'center',
-					}}>
-					<Box
-						sx={{
-							width: '38px',
-							height: '38px',
-							display: 'flex',
-							alignItems: 'center',
-							justifyContent: 'center',
-							borderRadius: '50%',
-							backgroundColor: '#415a77',
-							color: '#ffffff',
-						}}>
-						{notification.type == 'message' ? (
-							<MessageIcon fontSize='small' />
-						) : (
-							<RemoveCircleIcon fontSize='small' />
-						)}
-					</Box>
+      <Box
+        sx={{
+          width: "100%",
+          padding: "10px 10px",
+          borderBottom: "1px solid #f0f0f0",
+          cursor: "pointer",
+          ":hover": {
+            backgroundColor: "#f0f0f0",
+          },
+        }}
+        onClick={viewNotification}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Box
+            sx={{
+              width: "38px",
+              height: "38px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: "50%",
+              backgroundColor: "#415a77",
+              color: "#ffffff",
+            }}
+          >
+            {notification.type == "message" ? (
+              <MessageIcon fontSize="small" />
+            ) : (
+              <RemoveCircleIcon fontSize="small" />
+            )}
+          </Box>
 
-					<Box ml={0.6}>
-						<Typography sx={{fontSize: '0.8rem', color: '#1976d2'}}>
-							{notification.title}
-						</Typography>
-						<Typography sx={{fontSize: '0.75rem'}}>
-							{notification.description}
-						</Typography>
-					</Box>
-				</Box>
+          <Box
+            //ml={0.6}
+            sx={{
+              width: "calc(100% - 48px)",
+            }}
+          >
+            <Typography sx={{fontSize: "0.8rem", color: "#1976d2"}}>
+              {notification.title}
+            </Typography>
+            <Typography sx={{fontSize: "0.75rem"}}>
+              {notification.description}
+            </Typography>
+          </Box>
+        </Box>
 
-				<Box
-					sx={{
-						display: 'flex',
-						alignItems: 'center',
-						justifyContent: 'flex-end',
-					}}>
-					<Typography
-						sx={{
-							fontSize: '0.7rem',
-							color: '#006d77',
-						}}>
-						{dayjs(notification.createdAt).fromNow()}
-					</Typography>
-				</Box>
-			</Box>
-		</Box>
-	);
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-end",
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: "0.7rem",
+              color: "#006d77",
+            }}
+          >
+            {dayjs(notification.createdAt).fromNow()}
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default EachNotification;
---
+++ src/hsmodules/communication/sms/CreateSMS.js
@@ -0,0 +1,124 @@
+import {useContext, useState, useEffect, useCallback} from "react";
+import {Box, Grid, IconButton, Typography} from "@mui/material";
+import Input from "../../../components/inputs/basic/Input";
+import ModalBox from "../../../components/modal";
+//import {FormsHeaderText} from "../../../components/texts";
+import {useForm} from "react-hook-form";
+//import GlobalCustomButton from "../../../../components/buttons/CustomButton";
+import GlobalCustomButton from "../../../components/buttons/CustomButton";
+import {CKEditor} from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import DocViewer, {DocViewerRenderers} from "@cyntler/react-doc-viewer";
+import Drawer from "@mui/material/Drawer";
+import EmailIcon from "@mui/icons-material/Email";
+import SendIcon from "@mui/icons-material/Send";
+
+import {ObjectContext, UserContext} from "../../../context";
+import dayjs from "dayjs";
+import CustomTable from "../../../components/customtable";
+import {toast} from "react-toastify";
+
+import {pdfjs} from "react-pdf";
+import {Document, Page} from "react-pdf";
+import client from "../../../feathers";
+import {EmailsSourceList} from "../../CRM/components/deals/SendLink";
+import Textarea from "../../../components/inputs/basic/Textarea";
+import axios from "axios";
+//import {ContactsEmailSource, EmailsSourceList} from "../deals/SendLink";
+
+const CommunicationSMSCreate = ({closeModal}) => {
+  const smsServer = client.service("sms");
+  const emailServer = client.service("email");
+  const {user} = useContext(UserContext);
+  const {state, showActionLoader, hideActionLoader} = useContext(ObjectContext);
+
+  const {
+    register,
+    setValue,
+    reset,
+    handleSubmit,
+    formState: {errors},
+  } = useForm();
+
+  const handleSendEmail = async data => {
+    const facility = user.currentEmployee.facilityDetail;
+    showActionLoader();
+
+    //return console.log(document);
+
+    const document = {
+      message: data.message,
+      recipients: [data.phone_number],
+    };
+
+    const token = localStorage.getItem("feathers-jwt");
+
+    return axios
+      .post("https://healthstack-backend.herokuapp.com/sms", document, {
+        headers: {Authorization: `Bearer ${token}`},
+      })
+      .then(res => {
+        hideActionLoader();
+        closeModal();
+        toast.success(`SMS was sent successfully`);
+      })
+      .catch(err => {
+        hideActionLoader();
+        console.log(err);
+        //toast.error(`Sorry, Failed to send SMS ${err}`);
+      });
+
+    //axios.get("https://healthstack-backend.herokuapp.com/sms");
+
+    await smsServer
+      .create(document)
+      .then(res => {
+        hideActionLoader();
+        closeModal();
+        toast.success(`SMS was sent successfully`);
+      })
+      .catch(err => {
+        hideActionLoader();
+        console.log(err);
+        toast.error(`Sorry, Failed to send SMS ${err}`);
+      });
+  };
+
+  return (
+    <Box
+      sx={{
+        width: "600px",
+      }}
+    >
+      <Grid container spacing={1} mb={2}>
+        <Grid item lg={12} md={12} sm={12} xs={12}>
+          <Input
+            important
+            label="Phone Number"
+            register={register("phone_number", {require: "Please enter Name"})}
+            errorText={errors?.name?.number}
+          />
+        </Grid>
+
+        <Grid item lg={12} md={12} sm={12} xs={12}>
+          <Textarea
+            important
+            label="Message"
+            register={register("message", {require: "Please enter Subject"})}
+            errorText={errors?.message?.message}
+          />
+        </Grid>
+      </Grid>
+
+      <Box>
+        <GlobalCustomButton onClick={handleSubmit(handleSendEmail)}>
+          Send SMS
+          <SendIcon fontSize="small" sx={{marginLeft: "4px"}} />
+        </GlobalCustomButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default CommunicationSMSCreate;
