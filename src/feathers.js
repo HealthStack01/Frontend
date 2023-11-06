9ba20844fb45fc32ddbdf044e27bfb79ba8a99a3
apmis <simpa@apmis.ng>
2023-11-06 06:52:45 +0100

    updated server

--- src/feathers.js
+++ src/feathers.js
@@ -6,11 +6,12 @@ import authentication from "@feathersjs/authentication-client";
 const herok = "https://healthstack-backend.herokuapp.com";
 const azure = "https://hsbackend.azurewebsites.net";
 const url = "http://localhost:3035";
+const herok1="https://hs-backend-59a793db48af.herokuapp.com/"
 const API =
   process.env.NODE_ENV !== "production"
     ? url
     : "https://healthstack-backend.herokuapp.com";
-const socket = io(herok, {
+const socket = io(azure, {
   transports: ["websocket"],
   forceNew: true,
 });
